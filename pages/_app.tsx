913db188e6756d6009ee81cc797901c766d0ffb9
Limttugi <junhuk820@gmail.com>
2023-07-05 22:19:36 +0900

    docs: 기본 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import type { AppProps } from 'next/app'
 
+import '@/styles/reset.scss'
+import '@/styles/global.scss'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient()
+
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='ko'>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width' />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
